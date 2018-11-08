1e0c537aa703f0a4f7d8b4a4efb260dba44d3f24
Brooke <Brooke.horrocks7@gmail.com>
2018-11-08 13:59:14 -0700

    user model update with bcrypt

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
     username: {
@@ -30,5 +31,30 @@ const userSchema = new Schema({
         ref: "Reward"
     }]
 });
+
+userSchema.pre("save", function(next) {
+    const user = this;
+
+    if(!user.isModified("password")) return next();
+
+    bcrypt.hash(user.password, 10, (err, hash) => {
+        if(err) return next(err);
+        user.password = hash;
+        next();
+    });
+});
+
+userSchema.methods.checkPassword = function(passwordAttempt, callback){
+    bcrypt.compare(passwordAttempt, this.password, (err, isMatch) => {
+        if(err) return callback(err);
+        callback(null, isMatch);
+    });
+};
+
+userSchema.methods.withoutPassword = function(){
+    const user = this.toObject();
+    delete user.password;
+    return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
