64bbc2d4ab908ce0a0e038c9de8e8696939c1657
Brooke <Brooke.horrocks7@gmail.com>
2018-11-12 11:12:37 -0700

    user context setup

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,8 +1,13 @@
 import React, { Component, createContext } from "react";
 import axios from "axios";
 
-// TODO: Create user axios instance
-// TODO: Create interceptor
+const userAxios = axios.create();
+
+userAxios.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+});
 
 const UserData = createContext();
 
@@ -10,27 +15,65 @@ export default class UserContext extends Component {
     constructor(){
         super();
         this.state = {
-            // TODO: create state
+            user: JSON.parse(localStorage.getItem("user")) || {},
+            token: localStorage.getItem("token") || ""
         }
     }
 
-    login = () => {
-
+    componentDidMount(){
+        // TODO: get chores and rewards here?? getChores and getRewards are not local
     }
 
+    signup = (userInfo) => {
+        return userAxios.post("auth/signup", userInfo)
+            .then(response => {
+                const { user, token } = response.data
+                localStorage.setItem("token", token)
+                localStorage.setItem("user", JSON.stringify(user))
+                this.setState({
+                    user,
+                    token
+                });
+                return response;
+            });
+    };
+    login = (credentials) => {
+        return userAxios.post("auth/login", credentials)
+            .then(response => {
+                const { token, user } = response.data;
+                localStorage.setItem("token", token)
+                localStorage.setItem("user", JSON.stringify(user))
+                this.setState({
+                    user,
+                    token
+                });
+                //TODO: Get chores and rewards here?? getChores and getRewards are not local
+                return response;
+            });
+    };
+    logout = () => {
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+        this.setState({
+            user: {},
+            token: ""
+        });
+    };
+
     render() {
-        // TODO: bring other methods into props once written
         const props = {
             ...this.state,
-            login: this.login
+            signup: this.signup,
+            login: this.login,
+            logout: this.logout
         }
         return (
             <UserData.Provider value={props}>
                 {this.props.children}
             </UserData.Provider>
         )
-    }
-}
+    };
+};
 
 export const withUserContext = C => props => (
     <UserData.Consumer>
